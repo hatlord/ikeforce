f636a96a469779e092953fdb79d3403e0d3cd460
Hatlord <emailrich@gmail.com>
2021-02-21 13:20:43 +0000

    bugfixes

--- ikeclient.py
+++ ikeclient.py
@@ -6,6 +6,7 @@
 #You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import socket,OpenSSL,sys,struct
+import os
 try:
         import udp
 except:
